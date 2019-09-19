8b18faebb3a0ba909fc556b81b989315a7c37348
jeongjaeeom <jeongjae.eom@gmail.com>
2019-09-19 18:46:41 +0900

    Initial commit

---
+++ trabricks-spring-boot-starter-web/src/main/java/com/trabricks/web/configs/WebInterceptor.java
@@ -0,0 +1,79 @@
+package com.trabricks.web.configs;
+
+import java.util.Enumeration;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author eomjeongjae
+ * @since 2019-02-08
+ */
+@Slf4j
+public class WebInterceptor implements HandlerInterceptor {
+
+  private String getParameters(HttpServletRequest request) {
+    StringBuffer posted = new StringBuffer();
+    Enumeration<?> e = request.getParameterNames();
+    if (e != null) {
+      posted.append("?");
+    }
+    while (e.hasMoreElements()) {
+      if (posted.length() > 1) {
+        posted.append("&");
+      }
+      String curr = (String) e.nextElement();
+      posted.append(curr + "=");
+      if (curr.contains("password")
+          || curr.contains("pass")
+          || curr.contains("pwd")) {
+        posted.append("*****");
+      } else {
+        posted.append(request.getParameter(curr));
+      }
+    }
+    String ip = request.getHeader("X-FORWARDED-FOR");
+    String ipAddr = (ip == null) ? getRemoteAddr(request) : ip;
+    if (ipAddr != null && !ipAddr.equals("")) {
+      posted.append("&_psip=" + ipAddr);
+    }
+    return posted.toString();
+  }
+
+  private String getRemoteAddr(HttpServletRequest request) {
+    String ipFromHeader = request.getHeader("X-FORWARDED-FOR");
+    if (ipFromHeader != null && ipFromHeader.length() > 0) {
+      log.debug("ip from proxy - X-FORWARDED-FOR : " + ipFromHeader);
+      return ipFromHeader;
+    }
+    return request.getRemoteAddr();
+  }
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+      ModelAndView modelAndView) throws Exception {
+    String usrAgent = request.getHeader("User-Agent");
+    if (usrAgent != null && !usrAgent.isEmpty() && usrAgent.contains("MSIE")) {
+      String v = usrAgent.substring(usrAgent.indexOf("MSIE") + 4).trim();
+      v = v.substring(0, v.indexOf(";"));
+      if (Float.parseFloat(v) <= 9.0) {
+        modelAndView.setViewName("shared/barricade");
+      }
+    }
+    log.info("[postHandle][{}][{}{}]", request.getMethod(), request.getRequestURI(),
+        getParameters(request));
+  }
+
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+      Object handler, Exception ex) throws Exception {
+    if (ex != null) {
+      ex.printStackTrace();
+      log.info("[afterCompletion][{}][{}{}][exception: {}]", request.getMethod(),
+          request.getMethod(), request.getRequestURI(), ex);
+    }
+  }
+}
---
+++ trabricks-spring-boot-starter-web/src/main/java/com/trabricks/web/configs/WebMvcConfig.java
@@ -0,0 +1,72 @@
+package com.trabricks.web.configs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+/**
+ * @author eomjeongjae
+ * @since 2019-07-22
+ */
+@Slf4j
+@Configuration
+@AutoConfigureAfter(WebMvcAutoConfiguration.class)
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(webInterceptor());
+    registry.addInterceptor(localeChangeInterceptor());
+  }
+
+  @Bean
+  @ConditionalOnMissingBean(name = "webInterceptor")
+  public HandlerInterceptor webInterceptor() {
+    return new WebInterceptor();
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+    localeChangeInterceptor.setParamName("lang");
+    return localeChangeInterceptor;
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public LocaleResolver localeResolver() {
+    CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+    localeResolver.setCookieName("i18n");
+    localeResolver.setCookieMaxAge(-1);
+    localeResolver.setCookiePath("/");
+    return localeResolver;
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public MessageSourceAccessor messageSourceAccessor() {
+    return new MessageSourceAccessor(messageSource());
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public ReloadableResourceBundleMessageSource messageSource() {
+    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+    messageSource.setBasename("classpath:/i18n/messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
+
+}
