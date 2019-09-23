9f9f50fefc9805179765635286a6f688aad42e1e
jeongjaeeom <jeongjae.eom@gmail.com>
2019-09-23 20:10:25 +0900

    Fix

---
+++ trabricks-spring-boot-starter-web/src/main/java/com/trabricks/web/file/service/StorageService.java
@@ -0,0 +1,26 @@
+package com.trabricks.web.file.service;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author eomjeongjae
+ * @since 2019-09-23
+ */
+public interface StorageService {
+
+  void init();
+
+  void store(MultipartFile file);
+
+  Stream<Path> loadAll();
+
+  Path load(String filename);
+
+  Resource loadAsResource(String filename);
+
+  void deleteAll();
+
+}
---
+++ trabricks-spring-boot-starter-web/src/main/java/com/trabricks/web/pebble/PebbleViewExtention.java
@@ -0,0 +1,9 @@
+package com.trabricks.web.pebble;
+
+/**
+ * @author eomjeongjae
+ * @since 2019-09-23
+ */
+public class PebbleViewExtention {
+
+}
