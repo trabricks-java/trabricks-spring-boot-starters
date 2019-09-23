3cba337ab4be308ae5f9f5f0249c43d43e316138
jeongjaeeom <jeongjae.eom@gmail.com>
2019-09-24 00:43:41 +0900

    Fix

--- trabricks-spring-boot-starter-web/src/main/java/com/trabricks/web/pebble/PebbleViewExtention.java
+++ trabricks-spring-boot-starter-web/src/main/java/com/trabricks/web/pebble/PebbleViewExtention.java
@@ -1,9 +1,11 @@
 package com.trabricks.web.pebble;
 
+import com.mitchellbosecke.pebble.extension.AbstractExtension;
+
 /**
  * @author eomjeongjae
  * @since 2019-09-23
  */
-public class PebbleViewExtention {
+public class PebbleViewExtention extends AbstractExtension {
 
 }
