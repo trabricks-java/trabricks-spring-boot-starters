4ab6afd1c415fc9e6680c77b8d295dfc3b310a45
jeongjaeeom <jeongjae.eom@gmail.com>
2020-02-12 18:06:07 +0900

    Add SecurityAuditorAwareAutoConfiguration

---
+++ trabricks-spring-boot-starter-security/src/main/java/io/trabricks/boot/security/configs/SecurityAuditorAwareAutoConfiguration.java
@@ -0,0 +1,47 @@
+package io.trabricks.boot.security.configs;
+
+import java.util.Optional;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+
+/**
+ * @author eomjeongjae
+ * @since 2019-07-18
+ */
+@Configuration
+public class SecurityAuditorAwareAutoConfiguration {
+
+  @Bean
+  @ConditionalOnMissingBean
+  public AuditorAware<String> auditorProvider() {
+    return new SecurityAuditorAware();
+  }
+
+  public static class SecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+      return Optional.ofNullable(SecurityContextHolder.getContext())
+          .map(SecurityContext::getAuthentication)
+          .filter(Authentication::isAuthenticated)
+          .filter(authentication -> !AnonymousAuthenticationToken.class
+              .isAssignableFrom(authentication.getClass()))
+          .map(authentication -> {
+            if (OAuth2Authentication.class
+                .isAssignableFrom(authentication.getClass())) {
+              return authentication.getPrincipal().toString();
+            }
+            return ((UserDetails) authentication.getPrincipal()).getUsername();
+          });
+    }
+  }
+
+}
