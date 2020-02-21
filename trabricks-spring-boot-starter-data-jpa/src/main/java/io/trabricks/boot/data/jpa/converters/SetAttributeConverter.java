ef38f7607ad3481f1d02d86a45523f64b8bfdf3b
jeongjaeeom <jeongjae.eom@gmail.com>
2020-02-21 17:29:14 +0900

    Add SetAttributeConverter.java

---
+++ trabricks-spring-boot-starter-data-jpa/src/main/java/io/trabricks/boot/data/jpa/converters/SetAttributeConverter.java
@@ -0,0 +1,21 @@
+package io.trabricks.boot.data.jpa.converters;
+
+import java.util.Set;
+import javax.persistence.Converter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author eomjeongjae
+ * @since 2019-02-27
+ */
+@Slf4j
+@Converter(autoApply = true)
+public class SetAttributeConverter extends ObjectAttributeConverter<Set> {
+
+  @Override
+  protected Class<Set> getInstance() {
+    return Set.class;
+  }
+
+}
+
